7fabd2e9443dfab7c4b7c8212bb31c0182ac4edf
Alin <alin.cristian977@gmail.com>
2026-01-29 18:59:37 +0100

    Create Constants.cs

---
+++ FrenchPronouns/Constants.cs
@@ -0,0 +1,133 @@
+﻿namespace FrenchPronouns;
+
+public class Constants
+{
+    public static class PronounsCategories
+    {
+        public const string Personal = "Personal";
+        public const string Reflexive = "Reflexive";
+        public const string Possessive = "Possessive";
+        public const string Demonstrative = "Demonstrative";
+        public const string Relative = "Relative";
+        public const string Interrogative = "Interrogative";
+        public const string Indefinite = "Indefinite";
+        public const string Disjunctive = "Disjunctive";
+        public const string Reciprocal = "Reciprocal";
+    }
+
+    public static class Personal
+    {
+        public const string Je = "je";
+        public const string Tu = "tu";
+        public const string Il = "il";
+        public const string Elle = "elle";
+        public const string On = "on";
+        public const string Nous = "nous";
+        public const string Vous = "vous";
+        public const string Ils = "ils";
+        public const string Elles = "elles";
+    }
+
+    public static class Reflexive
+    {
+        public const string Me = "me";
+        public const string Te = "te";
+        public const string Se = "se";
+        public const string Nous = "nous";
+        public const string Vous = "vous";
+        public const string SePlural = "se"; // used for ils/elles
+    }
+
+    public static class Possessive
+    {
+        public const string Mon = "mon";
+        public const string Ma = "ma";
+        public const string Mes = "mes";
+        public const string Ton = "ton";
+        public const string Ta = "ta";
+        public const string Tes = "tes";
+        public const string Son = "son";
+        public const string Sa = "sa";
+        public const string Ses = "ses";
+        public const string Notre = "notre";
+        public const string Nos = "nos";
+        public const string Votre = "votre";
+        public const string Vos = "vos";
+        public const string Leur = "leur";
+        public const string Leurs = "leurs";
+    }
+
+    public static class Demonstrative
+    {
+        public const string Ce = "ce";
+        public const string Cet = "cet";
+        public const string Cette = "cette";
+        public const string Ces = "ces";
+        public const string Celui = "celui";
+        public const string Celle = "celle";
+        public const string Ceux = "ceux";
+        public const string Celles = "celles";
+    }
+
+    public static class Relative
+    {
+        public const string Qui = "qui";
+        public const string Que = "que";
+        public const string Dont = "dont";
+        public const string Où = "où";
+        public const string Lequel = "lequel";
+        public const string Laquelle = "laquelle";
+        public const string Lesquels = "lesquels";
+        public const string Lesquelles = "lesquelles";
+    }
+
+    public static class Interrogative
+    {
+        public const string Qui = "qui";
+        public const string Que = "que";
+        public const string Quoi = "quoi";
+        public const string Lequel = "lequel";
+        public const string Laquelle = "laquelle";
+        public const string Lesquels = "lesquels";
+        public const string Lesquelles = "lesquelles";
+        public const string Quel = "quel";
+        public const string Quelle = "quelle";
+        public const string Quels = "quels";
+        public const string Quelles = "quelles";
+    }
+
+    public static class Indefinite
+    {
+        public const string On = "on";
+        public const string Quelquun = "quelqu’un";
+        public const string Certains = "certains";
+        public const string Certaines = "certaines";
+        public const string Chacun = "chacun";
+        public const string Chacune = "chacune";
+        public const string Aucun = "aucun";
+        public const string Aucune = "aucune";
+        public const string Tout = "tout";
+        public const string Toute = "toute";
+        public const string Tous = "tous";
+        public const string Toutes = "toutes";
+    }
+
+    public static class Disjunctive
+    {
+        public const string Moi = "moi";
+        public const string Toi = "toi";
+        public const string Lui = "lui";
+        public const string Elle = "elle";
+        public const string Nous = "nous";
+        public const string Vous = "vous";
+        public const string Eux = "eux";
+        public const string Elles = "elles";
+    }
+
+    public static class Reciprocal
+    {
+        public const string Se = "se";
+        public const string LUnLEAutre = "l’un l’autre";
+        public const string LesUnsLesAutres = "les uns les autres";
+    }
+}
